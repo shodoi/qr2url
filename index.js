13c06e7b1896be71726a2f7b1d6289138ddca751
mywin <showweb1@gmail.com>
2021-05-06 23:03:30 +0900

    QRコードを選択したらURL返すところまで

---
+++ index.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+// import html2canvas from "node_modules/html2canvas/dist/html2canvas.min.js";
+// import { jsQR } from "node_modules/jsqr/dist/jsQR.js";
+
+// const code = jsQR(imageData.data, imageData.width, imageData.hegith);
+
+console.log("code");
